4165bed260b9b325f146c718cad04ef3426ddc08
Afia Rahman <rahmanmou26@gmail.com>
2024-07-16 12:27:04 +0600

    created API to add user data to database

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const userCollection = client.db('bankDB').collection('users');
+
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
